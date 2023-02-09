831f1473a6a90a82508f9c9850e7d5d760be2bd9
james <james@jamess-MacBook-Pro.local>
2023-02-09 16:40:11 +0000

    Fix rendering

--- prerender.sh
+++ prerender.sh
@@ -4,5 +4,5 @@ DESC=$(node -e 'console.log(require("./stats.js").description())')
 echo "Title: $TITLE"
 echo "Description: $DESC"
 
-sed -ri ".bak" "s/property=\"og:title\" content=\".*\"/property=\"og:title\" content=\"$TITLE\"/" index.html
-sed -ri ".bak" "s/property=\"og:description\" content=\".*\"/property=\"og:description\" content=\"$DESC\"/" index.html
+sed -ri ".bak" "s/property=\"og:title\" content=\"(.*)\"/property=\"og:title\" content=\"$TITLE\"/" index.html
+sed -ri ".bak" "s/property=\"og:description\" content=\"(.*)\"/property=\"og:description\" content=\"$DESC\"/" index.html
